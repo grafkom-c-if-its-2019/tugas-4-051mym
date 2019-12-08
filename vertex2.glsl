5d90d421683febe99b7d7bc2835c58503742a12b
M Yusuf Mukharom <42793951+051mym@users.noreply.github.com>
2019-12-08 22:15:34 +0700

    fix mugoho
    sad

---
+++ vertex2.glsl
@@ -0,0 +1,24 @@
+precision mediump float;
+
+attribute vec3 vPosition;
+attribute vec3 vNormal;
+attribute vec2 vTexCoord;
+
+varying vec2 fTexCoord;
+varying vec3 fPosition;
+varying vec3 fNormal;
+
+uniform mat4 projection;
+uniform mat4 view;
+uniform mat4 model;
+
+uniform mat3 normalMatrix;
+
+void main() {
+  gl_Position = projection * view * model * vec4(vPosition, 1.0);
+
+  fTexCoord = vTexCoord;
+  fPosition = vec3(view * model * vec4(vPosition, 1.0));
+  fNormal = normalMatrix * vNormal;
+}
+ 
